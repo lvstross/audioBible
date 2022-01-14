0c2c5fa910e3d25902ed27cd477a675238ebac20
Levi Gonzales <lvstross@gmail.com>
2022-01-13 17:43:24 -0700

    updated the loop script to add a wait time element between requests

--- genBook.py
+++ genBook.py
@@ -2,12 +2,18 @@
 import os
 import sys
 import time
+import random
+from datetime import datetime
 
 book = sys.argv[1]
 startChapter = sys.argv[2]
 endChapter = sys.argv[3]
 
-for it in range(startChapter, endChapter):
-    time.sleep(60 * 2)
+for it in range(int(startChapter), int(endChapter) + 1):
+    waitTime = random.randrange(3, 6)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print(f'Waiting {waitTime} minutes from {current_time} to continue operation')
+    time.sleep(60 * waitTime)
     os.system(f'python3 lsbible.py {book}+{it}')
     
